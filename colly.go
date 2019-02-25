6f86a6e05f4d742b79ae396ec7c4447ce1c08f35
jaebooker <jaesonbooker@gmail.com>
2019-02-24 19:33:45 -0800

    second

--- colly.go
+++ colly.go
@@ -12,7 +12,27 @@ c.Limit(&colly.LimitRule{
     //filter websites according to limitations
     DomainGlob: ()"craigslist.com"),
     //Delay for requests, so as not to get in trouble with the Domain Lords
-    Delay: 1 * time.Second
-    //delay at random, so cyber Judge Dredd doesn't try to take down GoCU
+    Delay: 1 * time.Second,
+    //delay at random, so cyber Frieza doesn't try to take down GoCU
     RandomDelay: 1 * time.Second,
+})
+c.Visit("https://craigslist.com")
+c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+    //get the link from anchor HTMLElement
+    link := e.Attr("href")
+    //get Colly collector to go to link
+    c.Visit(e.Request.AbsoluteURL(link))
+})
+c.OnHTML("title", func(e *colly.HTMLElement) {
+    fmt.Println(e.Text)
+})
+numVisited := 0
+c.OnRequest(func(r *colly.Request) {
+    if numVisited > 100 {
+        r.Abort()
+    }
+    numVisited++
+})
+c.OnResponse(func(r *colly.Response) {
+    fmt.Println(r.Body)
 })
