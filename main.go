e2ebce4850f0729153c386c73b3db84eb2811fba
jaebooker <jaesonbooker@gmail.com>
2019-03-03 23:34:03 -0800

    cleanup

--- main.go
+++ main.go
@@ -6,65 +6,17 @@ import (
     "golang.org/x/net/html"
     "golang.org/x/net/html/atom"
     "strings"
-    // "fmt"
-	// "net/http"
-    // "time"
-    // "io/ioutil"
-    // "log"
-    //"encoding/json"
     "github.com/labstack/echo"
-    //"github.com/labstack/echo/middleware"
 )
 type Item struct {
     Item string `json:"item"`
     URL string `json:"URL"`
 }
-// type Item struct {
-//     name string `json:"name"`
-//     URL string `json:"URL"`
-// }
-// type ItemArray struct {
-//     items string
-//     urls string
-// }
-// func crawler(u string) string {
-//     resp, err := http.Get(u)
-//     if err != nil {
-//         panic(err)
-//     }
-//     root, err := html.Parse(resp.Body)
-//     if err != nil {
-//         panic(err)
-//     }
-//     matcher := func(n *html.Node) bool {
-//             if n.DataAtom == atom.A && n.Parent != nil {
-//                 return scrape.Attr(n.Parent, "class") == "result-info"
-//             }
-//             return false
-//         }
-//     articles := scrape.FindAll(root, matcher) //<--change to find, with item matching
-//         for i, article := range articles {
-//             fmt.Printf("%2d %s (%s)\n", i, scrape.Text(article), scrape.Attr(article, "href"))
-//             //go deeper, in future
-//             // for n := range scrape.Attr(article,"href") {
-//             //     fmt.Printf("%2d %s\n", n, scrape.Text(article))
-//             // }
-//     }
-//     return "stringio"
-// }
 func scraper(c echo.Context) error {
     u := Item{}
     err := c.Bind(&u)
-    //u := c.Param("URL")
     fmt.Printf(u.Item)
-    // dataType := c.Param("data")
-    // if dataType == "json" {
-    //     return c.JSON(http.StatusOK, crawler(u))
-    // } else {
-    //     // return c.JSON(http.StatusBadRequest, map[string]string{
-    //     //     "error": "this is wrong",
-    //     // })
-    // }
+    fmt.Printf(u.URL)
 	resp, error := http.Get(u.URL)
 	if error != nil {
 		panic(err)
@@ -79,8 +31,7 @@ func scraper(c echo.Context) error {
     		}
     		return false
     }
-    articles := scrape.FindAll(root, matcher) //<--change to find, with item matching
-    //iarray := [10]ItemArray
+    articles := scrape.FindAll(root, matcher)
     var s [][]string
 	for i, article := range articles {
 		fmt.Printf("%2d %s (%s)\n", i, scrape.Text(article), scrape.Attr(article, "href"))
@@ -89,21 +40,10 @@ func scraper(c echo.Context) error {
             sa := []string{scrape.Text(article),scrape.Attr(article, "href")}
             s = append(s, sa)
         }
-        //iarray[i] = ItemArray{scrape.Text(article),scrape.Attr(article, "href")}
-		//go deeper, in future
-        // for n := range scrape.Attr(article,"href") {
-        //     fmt.Printf("%2d %s\n", n, scrape.Text(article))
-        // }
     }
-    //ia := ItemArray{"Stringy","Stringycheese"}
-    fmt.Printf("LOOK AT ME!")
-    fmt.Printf(u.Item)
-    fmt.Printf("I'M MISTY MEESEEKS!!")
-    fmt.Printf(u.URL)
     return c.JSON(http.StatusOK, map[string][][]string{
             "Item List": s,
     })
-	//return c.String(http.StatusOK, "Cookie with your meal, Sir?")
 }
 func main(){
     e := echo.New()
