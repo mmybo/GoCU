1aa3efb1e59595b4594a3b3f5a0dcb92d4f683ce
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-07 21:03:10 -0800

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,38 @@
 package main
+// A Go Crawler Utility API (GoCU)
+
+// Simply make a post request using JSON, with the following format:
+
+//      {
+//          "Item": [word you are searching for],
+//          "URL": [web address you wish to search for it in]
+//      }
+ 
+// And GoCU will return all pages containing that word, and the links to them:
+
+//     {
+//         "Item List": [
+//             "[Page Title]",
+//             "[URL Linking to Page]"
+//         ]
+//     }
+
+// Docs page can be found here: https://mmybo.github.io/GoCU/
+// Deployed: https://gocu.herokuapp.com/
+
+// GoCU is open source, so you can clone it and make it even more powerful!
+
+// To start off, simply clone the repo, using 'git clone https://github.com/mmybo/GoCU.git' into your terminal.
+
+// Next install the dependencies, using the following commands:
+// go get github.com/yhat/scrape
+// go get golang.org/x/net/html
+// go get golang.org/x/net/html/atom
+// go get github.com/labstack/echo
+
+// And that's it! After that, just build and run! (go build, and go run main.go)
+
+// Run it locally, or deploy your own!
 import (
     "fmt"
     "net/http"
